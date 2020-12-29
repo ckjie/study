b469a4cf27ab7d742685640059b039372ce8b4ef
蔡坤杰 <ck1083423574@gmail.com>
2020-12-29 17:59:18 +0800

    暂存

--- py/hello.py
+++ py/hello.py
@@ -174,3 +174,176 @@
 # print(aa(), aa(), 'func')		# 1  2
 
 # lambda：表示匿名函数，如：lambda x: x * x，冒号前表示参数，限制：只能有一个表达式，返回值为表达式的结果
+
+# 在python中，变量名类似 __xxx__ （双下划线开头、双下划线结尾）的，是特殊变量，是可以直接访问的，不是私有变量
+
+# 在python中，如果实例的变量名以 __ 开头，则变成了一个私有变量，只有内部可以访问，外部不能访问
+# class Student(object):
+# 	def __init__(self, name, score):
+# 		self.name = name
+# 		self.score = score
+# 		self.__name = name
+# 		self.__score = score
+
+# 	def print_val(self):
+# 		print('%s: %s' % (self.__name, self.__score))
+
+# 	def get_name(self):
+# 		return self.__name
+
+# 	def set_name(self, name):
+# 		self.__name = name
+# xm = Student('xiaoming', 66)
+# # print(xm, xm.name, xm.__name, 'xm')		# xm实例		xiaoming		报错（不能访问私有变量）
+# print(xm.get_name(), 'get_name')
+# xm.set_name('kangkang')
+# print(xm.get_name(), 'get_name')
+
+# class SS(object):
+# 	count = 0
+# 	def __init__(self, name):
+# 		SS.count += 1
+# 		self.name = name
+
+# if SS.count != 0:
+# 	print('not 0')		# is 0
+# else:
+# 	print('is 0')
+# a = SS('xiaoming')
+# if SS.count != 1:
+# 	print('not 1')
+# else:
+# 	print('is 1')		# is 1
+# b = SS('wangcai')
+# print(SS.count, 'count')		# 2
+
+# class Screen(object):
+# 	@property
+# 	def width(self):
+# 		return self._width
+# 	@width.setter
+# 	def width(self, val):
+# 		self._width = val
+# 	@property
+# 	def height(self):
+# 		return self._height
+# 	@width.setter
+# 	def height(self, val):
+# 		self._height = val
+# 	@property
+# 	def resolution(self):
+# 		return self._width * self._height
+# s = Screen()
+# s.width = 1024
+# s.height = 768
+# print('resolution =', s.resolution)
+
+# 通过 callable() 函数，可以判断一个对象是否是 “可调用” 对象
+# callable(max) => True 		callable([1, 2, 3]) => False
+
+# 枚举类，value属性自动赋给成员 int 常量，默认从 1 开始计数
+# from enum import Enum
+# Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+# for name, member in Month.__members__.items():
+# 	print(name, '=>', member, '=>', member.value)		# 打印对应月份及其值，值为 1-12
+# 如果需要更精确的空置枚举类型，可以从 Enum 派生出自定义类，@unique 装饰器可以检查保证没有重复值
+# from enum import Enum, unique
+# @unique
+# class Weekday(Enum):
+# 	Sun = 0
+# 	Mon = 1
+# 	Tue = 2
+# 	Wed = 3
+# 	Thu = 4
+# 	Fri = 5
+# 	Sat = 6
+# # print(Weekday.Tue, '=>', Weekday.Tue.value)
+# for name, member in Weekday.__members__.items():
+# 	print(name, '=>', member, '=>', member.value)
+
+# 浮点字符串调用 int() 方法时会报错！！ 如：int('6.6') => 报错
+
+# from functools import reduce
+# def str2num(s):
+# 	return int(float(s))
+# def calc(exp):
+#   ss = exp.split('+')
+#   ns = map(str2num, ss)
+#   return reduce(lambda acc, x: acc + x, ns)
+# def main():
+#   r = calc('100 + 200 + 345')
+#   print('100 + 200 + 345 =', r)
+#   r = calc('99 + 88 + 7.6')
+#   print('99 + 88 + 7.6 =', r)
+# main()
+
+# 凡是用 print() 来辅助查看的地方，都可以用断言 assert 来替代
+# 如： assert n != 0, 'n is zero'
+# assert 的意思是，表达式 n != 0 应该是True，否则，根据程序运行的逻辑，后面的代码肯定会出错
+# 在启动Python解释器的时候，可以用 -O 参数来关闭assert，如：python -O hello.py，此时，assert可以当做pass来看
+
+# logging：不会抛出错误，而且可以输出到文件，等级有debug、info、warning、error等
+# import logging
+# logging.basicConfig(level=logging.INFO)
+
+# s = '0'
+# n = int(s)
+# logging.info('n = %s' % n)
+# print(10 / n)
+
+# pdb.set_trace()：用于设置断点，需先引入 pdb
+# 调试时，可以键入 p 变量名 获取此时变量的值，键入 c 执行下一步
+
+# isdigit：用于判断字符串是否只有数字组成
+# a = '111'
+# b = '1.11'
+# print(a.isdigit(), b.isdigit(), 'aabb') # True false
+
+# read()：读取文件，可使用 read(size) 每次最多读取size个字节的内容，使用 readline() 可以每次读取每一行内容，使用 readlines() 一次读取所有内容，并按行返回list
+# 标识符 r 表示读（文本文件）
+# 二进制文件，用 rb 标识符
+# 写文件：标识符使用 w（文本文件） 或 wb（二进制文件），使用 a 表示追加写入（默认为删除后写入）
+# 要读取非 UTF-8 编码的文本文件，需要传入 encoding 参数，如：open(xxx, 'r', encoding='gbk')
+# 还可以传入一个 errors 参数，表示遇到编码错误后如何处理，最简单的方法是直接忽略：open(xxx, 'r', encoding='gbk', errors='ignore')
+# title() 方法：首字符大写，其余小写
+# with open('./file.txt', 'r') as f:
+# 	ls = f.readlines()
+# 	for aa in ls:
+# 		aa.replace('\n', '')
+# 		print(aa, '\n' in aa, aa.title())
+# with open('./file.txt', 'a') as f:
+# 	f.write('\nhello py')
+
+# 操作文件和目录
+# import os
+# print(os.name)		# nt（window操作系统）
+# # print(os.environ)
+# print(os.path.abspath('.'))		# 查看当前目录的绝对路径
+
+# JSON序列化
+# import json
+# json.dumps(dict_obj)		# 把dict序列化为json字符串
+# json.loads(json_str)		# 把json字符串反序列化
+# print(json.dumps(dict_obj, default=lambda obj: obj.__dict__))		# 把实例序列化为json字符串
+# 因为通常class的实例都有一个 __dict__属性，它就是一个dict，用来存储实例变量，也有少数例外，比如定义了 __slots__ 的class
+
+# 获取当前进程的ID
+# import os
+# print('Process (%s) start...' % os.getpid())
+
+# 多进程
+# 创建子进程时，只需要传入一个执行函数和函数的参数，创建一个Process实例，用start()方法启动，这样创建进程比fork()还要简单
+# join()方法可以等待子进程结束后再继续往下运行，通常用于进程间的同步
+# from multiprocessing import Process
+# import os
+# # 子进程要执行的代码
+# def run_proc(name):
+# 	print('Run child process %s (%s)...' % (name, os.getpid()))
+
+# if __name__ == '__main__':
+# 	print('Parent process %s' % os.getpid())
+# 	p = Process(target=run_proc, args=('test',))
+# 	print('Child process will start,')
+# 	p.start()
+# 	p.join()
+# 	print('Child process end')
